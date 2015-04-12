b2af22556f465f54d01b68809e369c11d169da18
Vitaly _Vi Shukela <vi0oss@gmail.com>
2015-04-12 23:06:42 +0300

    Add a test for parser

--- src/mkv/elements/parser/test.rs
+++ src/mkv/elements/parser/test.rs
@@ -1,20 +1,28 @@
-use super::Event;
-use super::EventsHandler;
+use super::*;
 
-
-/*struct GatherEvents<'a> {
-    accumulator : Vec<Event<'a> >,
-}
-
-impl<'a> EventsHandler for GatherEvents<'a> {
-    fn event(&mut self, e : Event){
-        self.accumulator.push(e);
+macro_rules! ehi {
+    ($x:ty, $ctr:ident, $e:ident, $c:block) => {
+        impl<'a> EventsHandler for $x {
+            fn event(&mut self, $e : Event) {*self.$ctr += 1; $c }
+            fn log(&mut self, m : &str) { println!("{}", m); }
+        }
     }
-    fn log(&mut self, m : &str) { println!("{}",m)  }
-}*/
-
+    
+}
+    
 #[test]
 fn element_parse_test_1() {
-    let b = &[255];
-    
+    let mut ctr = 0;
+    struct Q<'a> { ctr: &'a mut i32};
+    ehi!(Q<'a>, ctr, e, {
+        match e {
+            Event::Resync => (),
+            _ => panic!("Expected resync"),
+        }
+    });
+    {
+        let mut p = super::new(Q{ctr : &mut ctr});
+        p.feed_bytes(&[255, 255, 255, 255]);
+    }
+    assert_eq!(ctr, 1);
 }
