72058ebb463d8fe56788d3a16a72433d0de63dea
Vitaly _Vi Shukela <vi0oss@gmail.com>
2015-07-15 02:23:57 +0300

    Actualise for the nightly Rust

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![feature(convert)]
 #![feature(collections)]
 #![feature(slice_patterns)]
+#![feature(vec_push_all)]
+#![feature(append)]
 
 #![allow(non_camel_case_types)]
 #![allow(unused_imports)]
