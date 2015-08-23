4ab3635ece9b3c9540d7c14a0d1c83c80c52b9b0
Vitaly _Vi Shukela <vi0oss@gmail.com>
2015-08-24 01:32:14 +0300

    WiP

--- examples/event_dumper.rs
+++ examples/event_dumper.rs
@@ -1,11 +1,15 @@
+#![feature(result_expect)]
+#![feature(convert)]
 
 #![allow(non_camel_case_types)]
 #![allow(unused_imports)]
+
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 use std::io::Read;
 use std::io::Write;
+use std::env::args;
 
 use mkv::elements::parser::Parser;
 
@@ -22,11 +26,13 @@ const BSIZE : usize = 4096;
 fn main() {
     let _ = log::set_logger(|ll| { ll.set(log::LogLevelFilter::Debug); Box::new(SimpleLogger) });
 
-    let path = Path::new("q.mkv");
-    let mut f = match File::open(&path) {
-        Ok(x) => BufReader::new(x),
-        Err(_) => panic!("Failed to open file q.mkv"),
+    let mut reader : Box<Read> = match args().len() {
+        1 => Box::new(std::io::stdin()),
+        2 => Box::new(File::open(Path::new(args().nth(1).unwrap().as_str())).expect("Failed to open the file")),
+        _ => panic!("Usage: event_dumper [filename.mkv]")
     };
+    let mut f = BufReader::new(reader);
+   
     
     //let mut stdout = std::io::stdout();
     let element_logger = mkv::elements::parser::debug::DebugPrint::new(log::LogLevel::Info);
