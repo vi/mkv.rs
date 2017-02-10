273a1b14fb89a4a3f86a8b5393c41f89e6381ca2
Vitaly _Vi Shukela <vi0oss@gmail.com>
2017-02-10 06:18:17 +0300

    more things

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,41 @@ use nom::{IResult, ErrorKind, be_u8, be_u16, be_u32};
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct EbmlNumber {
-    raw_value: u64,
-    length: u8,
+    pub raw_value: u64,
+    pub length: u8,
 }
+
+type EbmlElementRawId = u64;
+
+pub struct EbmlElementHeader {
+    pub id: EbmlElementRawId,
+    pub len: Option<u64>,
+}
+
+
+impl EbmlNumber {
+    fn is_nan(&self) -> bool {
+        self.raw_value == (0b1 << (self.length * 7)) - 1
+    }
+    fn as_id(&self) -> EbmlElementRawId {
+        self.raw_value | (0b1 << (self.length * 7))
+    }
+    fn as_unsigned(&self) -> u64 {
+        self.raw_value
+    }
+}
+
+named!(pub ebml_header <EbmlElementHeader>,
+    do_parse!(
+        id: ebml_number >>
+        len: ebml_number >>
+        (EbmlElementHeader{
+            id: id.as_id(),
+            len: if len.is_nan() { None} else { Some(len.as_unsigned() )}
+        })
+    )
+);
+
 
 named!(pub ebml_number <EbmlNumber>,
     alt!(
@@ -69,198 +101,5 @@ named!(pub ebml_number <EbmlNumber>,
     )
 );
 
-impl EbmlNumber {
-    fn is_nan(&self) -> bool {
-        self.raw_value == (0b1 << (self.length * 7)) - 1
-    }
-    fn as_id(&self) -> u64 {
-        self.raw_value | (0b1 << (self.length * 7))
-    }
-}
 
-#[test]
-fn eun() {
-    assert_eq!(ebml_number(b"\x8A\x45\xDF\xA3"),
-               IResult::Done(&b"\x45\xDF\xA3"[..],
-                             EbmlNumber {
-                                 raw_value: 0x0A,
-                                 length: 1,
-                             }));
-    assert_eq!(ebml_number(b"\x4A\x45\xDF\xA3"),
-               IResult::Done(&b"\xDF\xA3"[..],
-                             EbmlNumber {
-                                 raw_value: 0x0A45,
-                                 length: 2,
-                             }));
-    assert_eq!(ebml_number(b"\x2A\x45\xDF\xA3"),
-               IResult::Done(&b"\xA3"[..],
-                             EbmlNumber {
-                                 raw_value: 0x0A45DF,
-                                 length: 3,
-                             }));
-    assert_eq!(ebml_number(b"\x1A\x45\xDF\xA3"),
-               IResult::Done(&b""[..],
-                             EbmlNumber {
-                                 raw_value: 0x0A45DFA3,
-                                 length: 4,
-                             }));
-    assert_eq!(ebml_number(b"\x09\x45\xDF\xA3\x01\x02\x03\x04"),
-               IResult::Done(&b"\x02\x03\x04"[..],
-                             EbmlNumber {
-                                 raw_value: 0x0145DFA301,
-                                 length: 5,
-                             }));
-    assert_eq!(ebml_number(b"\x05\x45\xDF\xA3\x01\x02\x03\x04"),
-               IResult::Done(&b"\x03\x04"[..],
-                             EbmlNumber {
-                                 raw_value: 0x0145DFA30102,
-                                 length: 6,
-                             }));
-    assert_eq!(ebml_number(b"\x03\x45\xDF\xA3\x01\x02\x03\x04"),
-               IResult::Done(&b"\x04"[..],
-                             EbmlNumber {
-                                 raw_value: 0x0145DFA3010203,
-                                 length: 7,
-                             }));
-    assert_eq!(ebml_number(b"\x01\x45\xDF\xA3\x01\x02\x03\x04"),
-               IResult::Done(&b""[..],
-                             EbmlNumber {
-                                 raw_value: 0x45DFA301020304,
-                                 length: 8,
-                             }));
-}
-
-#[test]
-fn t_isnan() {
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x7F,
-                       length: 1,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x7F,
-                       length: 2,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x00,
-                       length: 1,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x01,
-                       length: 1,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x7E,
-                       length: 1,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x3FFF,
-                       length: 2,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x3FFF,
-                       length: 3,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x3FFF,
-                       length: 4,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x3FFE,
-                       length: 2,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x0000,
-                       length: 2,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x1FFFFF,
-                       length: 3,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x1FFFFF,
-                       length: 4,
-                   }
-                   .is_nan(),
-               false);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x0FFFFFFF,
-                       length: 4,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x07FFFFFFFF,
-                       length: 5,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x03FFFFFFFFFF,
-                       length: 6,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x01FFFFFFFFFFFF,
-                       length: 7,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x00FFFFFFFFFFFFFF,
-                       length: 8,
-                   }
-                   .is_nan(),
-               true);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x00FFFFFFFFFFFFFE,
-                       length: 8,
-                   }
-                   .is_nan(),
-               false);
-}
-
-#[test]
-fn t_asid() {
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x0A,
-                       length: 1,
-                   }
-                   .as_id(),
-               0x8A);
-    assert_eq!(EbmlNumber {
-                       raw_value: 0x0A45DFA3,
-                       length: 4,
-                   }
-                   .as_id(),
-               0x1A45DFA3);
-
-}
-
-#[test]
-fn t_asid2() {
-    assert_eq!(ebml_number(b"\x1A\x45\xDF\xA3").unwrap().1.as_id(),
-               0x1A45DFA3);
-}
+mod test;
